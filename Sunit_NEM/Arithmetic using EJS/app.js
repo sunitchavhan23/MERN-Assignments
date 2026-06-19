8e9f4bb76c9b9588298b97896165dd26a5cfadcf
sunitchavhan23 <sunitchavan499@gmail.com>
2026-06-19 17:55:33 +0530

    BootStrap Cards & Arithmetic Operation using EJS Uploaded

--- Sunit_NEM/Arithmetic using EJS/app.js
+++ Sunit_NEM/Arithmetic using EJS/app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const app=express()
+const express = require("express");
+const app = express();
 
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 
-app.get("/add",(req,resp)=>{
-    const add=
-   
-})
+app.get("/", (req, res) => {
+    let result = null;
+    if (req.query.num1 && req.query.num2) {
+        const n1 = Number(req.query.num1);
+        const n2 = Number(req.query.num2);
+        result = {
+            add: n1 + n2,
+            sub: n1 - n2,
+            mul: n1 * n2,
+            div: n2 !== 0 ? n1 / n2 : "Cannot Divide by Zero"
+        };
+    }
+    res.render("index", { result });
+});
 
-app.listen(4000,()=>{
-    console.log("running");
-    
-})
+app.listen(4000, () => {
+    console.log("Running...");
+});
